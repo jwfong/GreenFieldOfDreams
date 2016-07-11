905548d5e4d8b242e3ba9a0b421543d6989317da
benjamin Patalano <Ben@benjamins-MacBook-Air.local>
2016-07-11 15:59:38 -0700

    Hooked data.service.js to the database with success! also worked on connecting it to activity-joined with Nick

--- ActivityTinder/app/data.service.js
+++ ActivityTinder/app/data.service.js
@@ -7,6 +7,15 @@
   DataService.prototype.getActivity = function() {
     return 'running';
   };
+  DataService.prototype.getJoinedActivities = function() {
+  	var activities = "no activities";
+  	firebase.database().ref('activities').once('value').then(function(snapshot) {
+  		console.log('inside promise');
+  		console.log(snapshot.val());
+  		return snapshot.val().test;
+	});
+	
+  };
   app.DataService = DataService;
 
 })(window.app = window.app || {});
