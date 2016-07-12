1fa1a0896c86c8609b52e543a9fad76e3fb9524f
Joshua Fong <jwfong89@gmail.com>
2016-07-12 15:05:14 -0700

    made minor changes

--- ActivityTinder/app/activity-login.component.js
+++ ActivityTinder/app/activity-login.component.js
@@ -11,9 +11,10 @@
   ActivityComponent.annotations = [
     new ng.core.Component({
       selector: 'logging-in',
-      template: `<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
-</fb:login-button>`
-
+      template: `
+      <div class="fb-login-button" data-max-rows="1" data-size="xlarge" 
+      data-show-faces="false" data-auto-logout-link="true"></div>
+      `
     })
   ];
 
