920fe72f662ef42fd35f292ca7448474a1ea39ec
nick_d <ngdaniele@gmail.com>
2016-07-12 13:07:45 -0700

    joined page now accepts multiple activites

--- ActivityTinder/app/activity-joined.component.js
+++ ActivityTinder/app/activity-joined.component.js
@@ -1,18 +1,46 @@
 (function(app) {
-
-  app.ActivityJoinedComponent = ActivityComponent;
-
+  // Create function to attach everything to and pass any injected service.
   function ActivityComponent(dataService) {
+    // Invoke service and save to components name space
     this.name = dataService.getActivity();
+    this.activities = dataService.getJoinedActivities();
   }
+  // Declare components injectables
   ActivityComponent.parameters = [
     app.DataService
   ];
+  // Create new DOM Space for Component
   ActivityComponent.annotations = [
+    // create new component
     new ng.core.Component({
+      // create selector for html
       selector: 'joined-activities',
-      template: '<h1>Activity: {{name}}</h1>'
+      // create template for DOM
+      template: 
+      `
+      <h1>Activity: {{name}}</h1>
+      <div>
+        <h1>Joined Events!</h1>
+        <div id="activityList">
+          <div class="activity" *ngFor="let activity of activities">
+            <h3>{{activity.eventName}}</h3>
+            <img id="activityPicture" [src]="activity.eventPhoto" width="200">
+            <p>Date: {{activity.eventDate}}</p>
+            <p>Time: {{activity.eventTime}}</p>
+            <p>Creator: {{activity.eventCreator}}</p>
+          </div>
+        </div>
+      </div>
+      `
     })
   ];
 
+  // Name and link component
+  app.ActivityJoinedComponent = ActivityComponent;
+
 })(window.app = window.app || {});
+
+
+// <ul>
+//   <li *ngFor="let activity of activity; let i = index">{{activity.eventName}}</li>
+// </ul>
