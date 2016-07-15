562a7d1d1a92ec255e617c1a590e5749d8875719
Joshua Fong <jwfong89@gmail.com>
2016-07-15 09:42:07 -0700

    moved log-in into component

--- ActivityTinder/app/activity-login.component.js
+++ ActivityTinder/app/activity-login.component.js
@@ -1,23 +1,29 @@
 (function(app) {
 
-  app.ActivityLoginComponent = ActivityComponent;
-
-  function ActivityComponent(dataService) {
-    this.name = dataService.getActivity();
-  }
-  ActivityComponent.parameters = [
-    app.DataService
-  ];
-  ActivityComponent.annotations = [
-    new ng.core.Component({
+  app.ActivityLoginComponent = 
+    ng.core.Component({
       selector: 'logging-in',
       template: `
-      <div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true" scope="email,public_profile,user_friends"></div>
+      <div class="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true" (submit)="login()" ></div>
       `
     })
-  ];
+    .Class({
+      constructor: function() {},
 
+      login: function() {
+        var provider = new firebase.auth.FacebookAuthProvider();
+        provider.addScope('user_friends');
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          var token = result.credential.accessToken;
+          var user = result.user;
+          consol.log(user)
+        }).catch(function(error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          var email = error.email;
+          var credential = error.credential;
+        });
+      }
+    })
 })(window.app = window.app || {});
-
-
 
